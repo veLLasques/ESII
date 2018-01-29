31afac74ee9b68fbc04c611b62db9375c86c512e
vellasques <diegovellasques@outlook.com>
2018-01-29 17:36:01 -0200

    Atributo string A3 adicionado

--- classA.cpp
+++ classA.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class A
 {
-	int A1;     // atributo int
-	float A2;    // atributo float
+	int A1;		// atributo int
+	float A2;	// atributo float
+	string A3;	// atributo string
 public:
 	void setA1(int _A1);
 	void setA2(float _A2);
 	int getA1();
 	float getA2();
+	void setA3(string _A3);
+	string getA3();
 };
 
 void A::setA1(int _A1) {
@@ -26,4 +30,12 @@ int A::getA1() {
 
 float A::getA2() {
 	return A2;
+}
+
+void A::setA3(string _A3) {
+	A3 = _A3;
+}
+
+string A::getA3() {
+	return A3;
 }
