0591d7006f8c4a406c8f0bfae10ddb2c8b46e650
vellasques <diegovellasques@outlook.com>
2018-01-29 17:43:01 -0200

    Metodo soma adicionado

--- classA.cpp
+++ classA.cpp
@@ -7,6 +7,7 @@ class A
 	int A1;		// atributo int
 	float A2;	// atributo float
 	string A3;	// atributo string
+	float soma;	// atributo para soma
 public:
 	void setA1(int _A1);
 	void setA2(float _A2);
@@ -14,6 +15,7 @@ public:
 	float getA2();
 	void setA3(string _A3);
 	string getA3();
+	float somaA1A2(A1, A2)
 };
 
 void A::setA1(int _A1) {
@@ -38,4 +40,9 @@ void A::setA3(string _A3) {
 
 string A::getA3() {
 	return A3;
+}
+
+float A::somaA1A2(A1, A2) {
+	soma = A1 + A2;
+	return soma;
 }
